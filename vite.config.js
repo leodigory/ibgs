d5acbbf69b362da0cc69e3c5a38fc96dd4e12f4a
leodigory <01leonardoaraujo@gmail.com>
2025-01-15 08:51:46 -0300

    up

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ibgs/', // Substitua pelo nome do repositório
+  base: '/ibgs', // Substitua pelo nome do repositório
 });
