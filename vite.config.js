211d782859b296d0f959e26720f0962e02ae4460
leodigory <01leonardoaraujo@gmail.com>
2025-01-14 22:34:04 -0300

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: '/', // Use './' para caminhos relativos
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // Pasta de saída do build
-  },
-});
+export default {
+  base: './', // Usa caminhos relativos
+};
