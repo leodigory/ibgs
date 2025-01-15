d67fed8304ca5ed96e8cefe287725668a76cc928
leodigory <01leonardoaraujo@gmail.com>
2025-01-15 09:27:52 -0300

    up

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ou use o caminho relativo apropriado, dependendo da sua estrutura
 });
