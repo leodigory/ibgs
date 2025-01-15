feaa7c09ce929422d6acbce50f6e8e67c44b73f5
leodigory <01leonardoaraujo@gmail.com>
2025-01-14 22:18:55 -0300

    up

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // ou './' para caminhos relativos
+  base: '/', // Use './' para caminhos relativos
   plugins: [react()],
   build: {
-    outDir: 'dist', // Pasta onde os arquivos de build serão gerados
-    sourcemap: true, // Gera sourcemaps para depuração
-    emptyOutDir: true, // Limpa a pasta de build antes de gerar novos arquivos
+    outDir: 'dist', // Pasta de saída do build
   },
-
 });
