f351c10f572658bea974dfce2ba6541328d5cfa9
leodigory <01leonardoaraujo@gmail.com>
2025-01-17 20:50:51 -0300

    up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { auth } from '../firebase'; // Importa o auth do Firebase
+import { useNavigate } from 'react-router-dom'; // Para redirecionar após o logout
 import { getUserRole } from '../auth'; // Importa a função para buscar o role
 import SideMenu from '../components/SideMenu';
 import './Home.css';
 
 function Home() {
   const [role, setRole] = useState('guest'); // Estado para armazenar o role
+  const navigate = useNavigate(); // Hook para redirecionamento
 
   useEffect(() => {
-    // Função para buscar o role do usuário logado
-    const fetchUserRole = async () => {
-      const user = auth.currentUser; // Pega o usuário logado
+    // Função para verificar a autenticação do usuário
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
       if (user) {
+        // Se o usuário estiver autenticado, busca o role
         const userRole = await getUserRole(user.uid); // Busca o role do Firestore
         setRole(userRole); // Atualiza o estado com o role
+      } else {
+        // Se o usuário não estiver autenticado, redireciona para a página de login
+        navigate('/');
       }
-    };
+    });
 
-    fetchUserRole(); // Chama a função ao carregar o componente
-  }, []);
+    // Limpa o listener ao desmontar o componente
+    return () => unsubscribe();
+  }, [navigate]);
 
   return (
     <div className="home-container">
