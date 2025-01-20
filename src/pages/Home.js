fcf3a41490e91d3fc1ba768e3f2206731b231c37
leodigory <01leonardoaraujo@gmail.com>
2025-01-20 20:24:06 -0300

    up modifique HOME

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,48 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { auth } from '../firebase';
-import { getUserRole, getUserData } from '../auth';
+import React from 'react';
 import SideMenu from '../components/SideMenu';
+import { useUserData } from '../hooks/useUserData'; // Importe o hook personalizado
 import './Home.css';
 
 function Home() {
-  const [role, setRole] = useState('guest');
-  const [userName, setUserName] = useState('');
-  const [userPhoto, setUserPhoto] = useState('');
-  const [userId, setUserId] = useState(null);
-  const location = useLocation();
-
-  useEffect(() => {
-    const fetchUserData = async () => {
-      const user = auth.currentUser;
-      if (user) {
-        const userRole = await getUserRole(user.uid);
-        const userData = await getUserData(user.uid);
-        setRole(userRole);
-        setUserName(userData.name);
-        setUserPhoto(userData.photoURL || '/default-profile.png');
-        setUserId(user.uid);
-      }
-    };
-
-    // Se houver dados no estado de navegação, use-os
-    if (location.state) {
-      setRole(location.state.role);
-      setUserName(location.state.userName);
-      setUserPhoto(location.state.userPhoto || '/default-profile.png');
-      setUserId(location.state.userId);
-    } else {
-      // Caso contrário, busque os dados do Firestore
-      fetchUserData();
-    }
-  }, [location.state]);
+  // Usando o hook para obter os dados do usuário
+  const { role, userName, userPhoto, userId } = useUserData();
 
   return (
     <div className="home-container">
       {/* Passa as informações para o SideMenu */}
       <SideMenu role={role} userName={userName} userPhoto={userPhoto} userId={userId} />
+
+      {/* Conteúdo da página */}
       <h1>Bem-vindo à página inicial, <strong>{userName}</strong>!</h1>
       <p>Você está logado como: <strong>{role}</strong></p>
+
+      {/* Mensagens condicionais com base no role */}
       {role === 'admin' && <p>Acesso total ao sistema.</p>}
       {role === 'pastor' && <p>Acesso total ao sistema.</p>}
       {role === 'financeiro' && <p>Acesso ao setor financeiro.</p>}
